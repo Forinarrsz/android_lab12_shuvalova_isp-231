1b431ddf4b81ed59de43e35220f605ac5ddb5e7d
dazzes4545 <dazzes7619@gmail.com>
2026-03-18 15:16:37 +0300

    Saved

--- app/src/main/java/com/shuvalova/myapplication/MainActivity.kt
+++ app/src/main/java/com/shuvalova/myapplication/MainActivity.kt
@@ -1,35 +1,184 @@
 package com.shuvalova.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.shuvalova.myapplication.model.Grade
 import com.shuvalova.myapplication.ui.theme.GradeClickerTheme
-
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.shuvalova.myapplication.data.Datasource
+private const val TAG = "MainActivity"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate Called")
         enableEdgeToEdge()
         setContent {
             GradeClickerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {GradeClickerApp(grades = Datasource.gradeList) }
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
 }
 
+@Composable
+fun GradeClickerApp(grades: List<Grade>) {
+    var points by rememberSaveable { mutableStateOf(0) }
+    var clicks by rememberSaveable { mutableStateOf(0) }
+    val currentGrade = determineGradeToShow(grades, points)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(16.dp)
+        )
+        Image(
+            painter = painterResource(currentGrade.imageId),
+            contentDescription = null,
+            modifier = Modifier
+                .size(dimensionResource(R.dimen.image_size))
+                .clickable{
+                    points += currentGrade.pointsPerClick
+                    clicks++
+                },
+            contentScale = ContentScale.Fit
+        )
+        TransactionInfo(
+            points = points,
+            clicks = clicks
+        )
+    }
+}
+
+@Composable
+fun TransactionInfo(
+    points: Int,
+    clicks: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .padding(16.dp)) {
+        Row(
+            modifier= Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.points_earned),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = points.toString(),
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.total_clicks),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                clicks.toString(),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+@Preview
+@Composable
+fun GradeClickerPreview() {
+    GradeClickerTheme {
+        GradeClickerApp(grades = Datasource.gradeList)
+    }
+}
+fun determineGradeToShow(
+    grades: List<Grade>,
+    points: Int
+): Grade {
+    var gradeToShow = grades.first()
+    for (grade in grades) {
+        if (points >= grade.threshold) {
+            gradeToShow = grade
+        } else {
+            break
+        }
+
+    }
+    return gradeToShow
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
